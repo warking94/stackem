442e506ede1a12315b3584183c68996cf3a94a9f
warking94 <bradley@bkwt.co.uk>
2026-01-21 13:41:21 +0000

    Add logic to add habits to a list
     and display to the user

--- js/script.js
+++ js/script.js
@@ -1,29 +1,47 @@
 const addHabitButton = document.getElementById('add-habit-button');
 const noHabitsMessage = document.getElementById('noHabitsMessage');
 const clearLocalStorage = document.getElementById('clear-local-storage');
+const habitsList = document.getElementById('habitsList');
 
 function addHabitToLocalStorage(habit) {
-    localStorage.setItem('habits', JSON.stringify(habit));
+    let oldHabits = localStorage.getItem('habits')
+    if (oldHabits == null) {
+        localStorage.setItem('habits', JSON.stringify([habit]))
+    } else {
+        oldHabits = JSON.parse(oldHabits)
+        oldHabits.push(habit)
+        localStorage.setItem('habits', JSON.stringify(oldHabits))
+    }
+    checkHabits();
 }
 
-function showHabits() {
-    noHabitsMessage.style.display = 'none';
-    const habits = localStorage.getItem('habits')
-    document.getElementById('habitsList').textContent = habits
+function checkHabits() {
+    if (localStorage.getItem('habits') == null) {
+        noHabitsMessage.style.display = 'block';
+        console.log('no habits found');
+        habitsList.style.display = 'none';
+    } else {
+        let habits = localStorage.getItem('habits');
+        habits = JSON.parse(habits);
+        console.log('habits found');
+        noHabitsMessage.style.display = 'none';
+        // for items in habit list populate habitsList
+        habitsList.innerHTML = '';
+        habits.forEach(habit => {
+            const li = document.createElement('li');
+            li.textContent = habit;
+            habitsList.appendChild(li)
+        })
+        habitsList.style.display = 'block';
+    }
+};
 
-}
+checkHabits()
 
 addHabitButton.addEventListener('click', () => {
     console.log('add habit button clicked');
     const habitInput = document.getElementById('habitInput');
-    if (habitInput.value.trim() === '') {
-        alert('habit input is empty');
-    } else {
-        console.log(habitInput.value);
-        addHabitToLocalStorage(habitInput.value);
-        habitInput.value = '';
-        showHabits();
-    }
+    addHabitToLocalStorage(habitInput.value);
 });
 
 clearLocalStorage.addEventListener('click', () => {
